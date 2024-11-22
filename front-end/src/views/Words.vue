b6ef7800249514a7daccc90928e56dc5182d6977
David <134694091+lephucdatl6@users.noreply.github.com>
2024-11-22 18:27:10 +0700

    Adding export file

--- front-end/src/views/Words.vue
+++ front-end/src/views/Words.vue
@@ -1,25 +1,39 @@
 <template>
     <div>
-        <h1>Words</h1>
+        <div style="display: flex; justify-content: space-between; align-items: center;">
+            <h1>Words</h1>
+            <button @click="exportWords" class="ui primary button">
+                <i class="download icon"></i> Export
+            </button>
+        </div>
         <table id="words" class="ui celled compact table">
             <thead>
                 <tr>
                     <th><i class="united kingdom flag"></i> English</th>
-                    <th><i class="de flag"></i>German</th>
-                    <th><i class="ru flag"></i>Russian</th>
+                    <th><i class="de flag"></i> German</th>
+                    <th><i class="ru flag"></i> Russian</th>
                     <th colspan="4"></th>
                 </tr>
             </thead>
-            <tr v-for="(word, i) in this.words" :key="i">
+            <tr v-for="(word, i) in words" :key="i">
                 <td>{{ word.english }}</td>
-                <td> {{ word.german }}</td>
-                <td> {{ word.russian }}</td>
+                <td>{{ word.german }}</td>
+                <td>{{ word.russian }}</td>
                 <td width="75" class="center aligned">
-                <router-link :to="{name: 'show', params: {id: word._id}}"><i class="eye icon"></i>Show</router-link></td>
+                    <router-link :to="{name: 'show', params: {id: word._id}}">
+                        <i class="eye icon"></i> Show
+                    </router-link>
+                </td>
                 <td width="75" class="center aligned">
-                <router-link :to="{name: 'edit', params: {id: word._id}}"><i class="edit icon"></i>Edit</router-link></td>
+                    <router-link :to="{name: 'edit', params: {id: word._id}}">
+                        <i class="edit icon"></i> Edit
+                    </router-link>
+                </td>
                 <td width="75" class="center aligned" @click.prevent="onDestroy(word._id)">
-                <a :href="`words/${word._id}`"><i class="trash icon"></i>Destroy</a></td>
+                    <a :href="`words/${word._id}`">
+                        <i class="trash icon"></i> Destroy
+                    </a>
+                </td>
             </tr>
         </table>
     </div>
@@ -41,9 +55,25 @@ export default {
             const sure = window.confirm('Are you sure?');
             if (!sure) return;
             await api.deleteWord(id);
-            this.flash('Word deleted sucessfully!', 'success');
+            this.flash('Word deleted successfully!', 'success');
             const newWords = this.words.filter(word => word._id !== id);
             this.words = newWords;
+        },
+        exportWords() {
+            // Create a CSV string
+            const csvContent = [
+                ['English', 'German', 'Russian'],
+                ...this.words.map(word => [word.english, word.german, word.russian])
+            ]
+                .map(row => row.join(','))
+                .join('\n');
+
+            // Create a blob and a link
+            const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = 'words.csv';
+            link.click();
         }
     },
     async mounted() {
