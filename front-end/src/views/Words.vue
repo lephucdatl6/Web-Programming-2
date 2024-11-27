e27e6330789e2b109810ac82a6036cd0af91d05a
lephucdatl6 <lephucdat2004>
2024-11-27 21:10:37 +0700

    Adding clear all option

--- front-end/src/views/Words.vue
+++ front-end/src/views/Words.vue
@@ -1,83 +1,99 @@
 <template>
     <div>
-        <div style="display: flex; justify-content: space-between; align-items: center;">
-            <h1>Words</h1>
-            <button @click="exportWords" class="ui primary button">
-                <i class="upload icon"></i> Export
-            </button>
-        </div>
-        <table id="words" class="ui celled compact table">
-            <thead>
-                <tr>
-                    <th><i class="united kingdom flag"></i> English</th>
-                    <th><i class="de flag"></i> German</th>
-                    <th><i class="ru flag"></i> Russian</th>
-                    <th colspan="4"></th>
-                </tr>
-            </thead>
-            <tr v-for="(word, i) in words" :key="i">
-                <td>{{ word.english }}</td>
-                <td>{{ word.german }}</td>
-                <td>{{ word.russian }}</td>
-                <td width="75" class="center aligned">
-                    <router-link :to="{name: 'show', params: {id: word._id}}">
-                        <i class="eye icon"></i> Show
-                    </router-link>
-                </td>
-                <td width="75" class="center aligned">
-                    <router-link :to="{name: 'edit', params: {id: word._id}}">
-                        <i class="edit icon"></i> Edit
-                    </router-link>
-                </td>
-                <td width="75" class="center aligned" @click.prevent="onDestroy(word._id)">
-                    <a :href="`words/${word._id}`">
-                        <i class="trash icon"></i> Destroy
-                    </a>
-                </td>
-            </tr>
-        </table>
+      <div style="display: flex; justify-content: space-between; align-items: center;">
+        <h1>Words</h1>
+            <div style="display: flex; gap: 10px;">
+                <button @click="destroyAllData" class="ui negative button">
+                    <i class="trash icon"></i> Clear All
+                </button>
+
+                <button @click="exportWords" class="ui primary button">
+                    <i class="upload icon"></i> Export
+                </button>
+            </div>
+      </div>
+      
+      <table id="words" class="ui celled compact table">
+        <thead>
+          <tr>
+            <th><i class="united kingdom flag"></i> English</th>
+            <th><i class="de flag"></i> German</th>
+            <th><i class="ru flag"></i> Russian</th>
+            <th colspan="4"></th>
+          </tr>
+        </thead>
+        <tr v-for="(word, i) in words" :key="i">
+          <td>{{ word.english }}</td>
+          <td>{{ word.german }}</td>
+          <td>{{ word.russian }}</td>
+          <td width="75" class="center aligned">
+            <router-link :to="{ name: 'show', params: { id: word._id } }">
+              <i class="eye icon"></i> Show
+            </router-link>
+          </td>
+          <td width="75" class="center aligned">
+            <router-link :to="{ name: 'edit', params: { id: word._id } }">
+              <i class="edit icon"></i> Edit
+            </router-link>
+          </td>
+          <td width="75" class="center aligned" @click.prevent="onDestroy(word._id)">
+            <a :href="`words/${word._id}`">
+              <i class="trash icon"></i> Destroy
+            </a>
+          </td>
+        </tr>
+      </table>
     </div>
-</template>
-
-<script>
-import router from '@/router';
-import { api } from '../helpers/helpers';
-
-export default {
+  </template>
+  
+  <script>
+  import router from '@/router';
+  import { api } from '../helpers/helpers';
+  
+  export default {
     name: 'words',
     data() {
-        return {
-            words: []
-        };
+      return {
+        words: []
+      };
     },
     methods: {
-        async onDestroy(id) {
-            const sure = window.confirm('Are you sure?');
-            if (!sure) return;
-            await api.deleteWord(id);
-            this.flash('Word deleted successfully!', 'success');
-            const newWords = this.words.filter(word => word._id !== id);
-            this.words = newWords;
-        },
-        exportWords() {
-            // Create a CSV string
-            const csvContent = [
-                ['English', 'German', 'Russian'],
-                ...this.words.map(word => [word.english, word.german, word.russian])
-            ]
-                .map(row => row.join(','))
-                .join('\n');
-
-            // Create a blob and a link
-            const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
-            const link = document.createElement('a');
-            link.href = URL.createObjectURL(blob);
-            link.download = 'words.csv';
-            link.click();
+      async onDestroy(id) {
+        const sure = window.confirm('Are you sure?');
+        if (!sure) return;
+        await api.deleteWord(id);
+        this.flash('Word deleted successfully!', 'success');
+        this.words = this.words.filter(word => word._id !== id);
+      },
+      async destroyAllData() {
+        const sure = window.confirm('Are you sure you want to delete ALL words?');
+        if (!sure) return;
+  
+        for (const word of this.words) {
+          await api.deleteWord(word._id);
         }
+  
+        this.words = [];
+        this.flash('All words deleted successfully!', 'success');
+      },
+      exportWords() {
+        const csvContent = [
+          ['English', 'German', 'Russian'],
+          ...this.words.map(word => [word.english, word.german, word.russian])
+        ]
+          .map(row => row.join(','))
+          .join('\n');
+  
+        const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = 'words.csv';
+        link.click();
+      }
     },
     async mounted() {
-        this.words = await api.getWords();
+      this.words = await api.getWords();
     }
-};
-</script>
+  };
+  </script>
+  
