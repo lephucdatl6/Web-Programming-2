6f5b61ef1fa5ea2c353d019f8da7cd2d4ea4fa56
lephucdatl6 <lephucdat2004>
2024-11-27 20:27:22 +0700

    Update description

--- front-end/src/views/AboutMe.vue
+++ front-end/src/views/AboutMe.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>About Dat</h1>
         <p>
-            This page belong to Dat
+            This page belongs to Dat. This system supports three languages: English, German, and Russian and it includes a test feature that randomizes questions across these three languages.
         </p>
     </div>
 </template>
