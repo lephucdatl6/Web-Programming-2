ddfaed3a6a4be0727b84075ff785df2445cdd0c6
lephucdatl6 <lephucdat2004>
2024-11-29 15:08:04 +0700

    Changing language order in Show.vue

--- front-end/src/views/Show.vue
+++ front-end/src/views/Show.vue
@@ -4,16 +4,16 @@
   
       <div class="ui labeled input fluid">
         <div class="ui label">
-          <i class="germany flag"></i> German
+          <i class="united kingdom flag"></i> English
         </div>
-        <input type="text" readonly  :value="word.german"/>
+        <input type="text" readonly  :value="word.english"/>
       </div>
 
       <div class="ui labeled input fluid">
         <div class="ui label">
-          <i class="united kingdom flag"></i> English
+          <i class="germany flag"></i> German
         </div>
-        <input type="text" readonly  :value="word.english"/>
+        <input type="text" readonly  :value="word.german"/>
       </div>
 
       <div class="ui labeled input fluid">
